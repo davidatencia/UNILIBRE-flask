c55f886b66287b18434192aa0d7609c8eef1808b
David <davidatencia2000@gmail.com>
2019-03-23 16:53:34 -0500

    inicial

---
+++ Python/Class.py
@@ -0,0 +1,12 @@
+class Operaciones:
+
+    def __init__(self, num1 , num2):
+        self.num2 = num2
+    def suma(self):
+        return self.num1+self.num2
+    def resta(self):
+        return self.num1-self.num2
+
+Op = Operaciones(5,4)
+print (Op.suma())
+print (Op.resta())
---
+++ Python/Dic.py
@@ -0,0 +1,5 @@
+ages = {"Alice": 22, "Bob":24, "Juan": 24}
+
+ages["Brayan"] = 30 
+ages["Alice"] += 1
+print(ages)
